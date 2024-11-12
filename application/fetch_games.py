51bc2ca92474b78fa486f08200363e25c0e5af71
Mateusz <115219630+Cent0l@users.noreply.github.com>
2024-11-12 10:46:46 +0100

    First and simpiest vertion of my program
    Just to search games

--- application/fetch_games.py
+++ application/fetch_games.py
@@ -1,6 +1,7 @@
-
+# application/fetch_games.py
 from api.steam_api_service import SteamAPIService
 from core.game import Game
+
 
 class FetchGames:
     def __init__(self, steam_api_service: SteamAPIService):
@@ -9,3 +10,14 @@ class FetchGames:
     async def execute(self):
         games_data = await self.steam_api_service.fetch_game_list()
         return [Game(appid=game['appid'], name=game['name']) for game in games_data]
+
+    async def get_game_by_appid(self, appid):
+        games = await self.execute()
+        for game in games:
+            if game.appid == appid:
+                return game
+        return None
+
+    async def get_games_by_name(self, name):
+        games = await self.execute()
+        return [game for game in games if name.lower() in game.name.lower()]
--- presentation/main.py
+++ presentation/main.py
@@ -1,20 +1,46 @@
+# presentation/main.py
 import asyncio
 from application.fetch_games import FetchGames
 from api.steam_api_service import SteamAPIService
 
 
 async def main():
-
     steam_api_service = SteamAPIService()
     fetch_games_use_case = FetchGames(steam_api_service)
 
+    print("Search options:")
+    print("1 - Show all available games")
+    print("2 - Search game by appid")
+    print("3 - Search games by name")
 
-    games = await fetch_games_use_case.execute()
+    choice = input("Choose an option number: ")
 
+    if choice == "1":
+        games = await fetch_games_use_case.execute()
+        print("List of games:")
+        for game in games[:]:  #daj liczbe po : [:5] zeby ilosc wyswietlic
+            print(game)
 
-    print("list of games:")
-    for game in games[:5]:  # Wyświetl konkretne gry
-        print(game)
+    elif choice == "2":
+        appid = int(input("Enter the game appid: "))
+        game = await fetch_games_use_case.get_game_by_appid(appid)
+        if game:
+            print(f"\nFound game: {game}")
+        else:
+            print("\nNo game found with the given appid.")
+
+    elif choice == "3":
+        name = input("Give game name or part of it: ")
+        games = await fetch_games_use_case.get_games_by_name(name)
+        if games:
+            print("\nFound games:")
+            for game in games:
+                print(game)
+        else:
+            print("\nNo games found with the given name.")
+
+    else:
+        print("\nInvalid option.")
 
 
 if __name__ == "__main__":
